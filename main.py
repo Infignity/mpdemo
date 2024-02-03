df90791e9fb07cc431c05736de8ff5ed6076ab85
Yashwant Jodha <ysjodha1@gmail.com>
2024-02-03 14:07:39 +0530

    add icp

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 from fastapi import FastAPI
 from apollo import Apollo
+import json
 
 app = FastAPI()
 
 @app.get("/random_apollo_search")
 def random_apollo(q="john"):
     "Return 25 random data points from Apollo."
-    apollo = Apollo()
-    payload = {
-        'finder_view_id': '5b6dfc5a73f47568b2e5f11c',
-        'q_keywords': q,
-        'page': 1,
-        'display_mode': 'explorer_mode',
-        'per_page': 25,
-        'open_factor_names': [],
-        'num_fetch_result': 1,
-        'context': 'people-index-page',
-        'show_suggestions': False,
-        'ui_finder_random_seed': '2fw3w6c21zm',
-        'cacheKey': 1706784155092,
-    }
+    return
 
-    return apollo.search(payload=payload)
+@app.get("/icp")
+def icp(q="spice"):
+    "Return preset icp if query matches the input."
+    icp_files = ['spice_icp']
+    for f in icp_files:
+        if q in f:
+            with open(f'files/{f}.json') as fp:
+                return json.load(fp)
+    return []
